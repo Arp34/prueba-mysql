4bb28cea0dcd5b9fac807c83e9b85d81d913c5ec
Arp34 <axeldrp3407@gmail.com>
2026-07-06 20:43:48 -0500

    prueba de desempeño Riwi MySQL

--- queries.sql
+++ queries.sql
@@ -1,31 +1,35 @@
- -- Query 1: Available inventory by product
+-- query 1: available inventory by product
 select
     product_name,
     stock
 from eco_product
 order by stock desc;
 
--- Query 2: Order history by city
+-- query 2: order history by city
 select
-    c.city_name,
-    count(o.id_order) as total_orders
-from eco_order o
-join eco_customer cu on o.id_customer = cu.id_customer
-join eco_city c on cu.id_city = c.id_city
-group by c.city_name
+    eco_city.city_name,
+    count(eco_order.id_order) as total_orders
+from eco_order
+join eco_customer
+    on eco_order.id_customer = eco_customer.id_customer
+join eco_city
+    on eco_customer.id_city = eco_city.id_city
+group by eco_city.city_name
 order by total_orders desc;
 
--- Query 3: Total sales by category
+-- query 3: total sales by category
 select
-    ca.category_name,
-    sum(od.quantity * od.historical_price) as total_sales
-from eco_order_detail od
-join eco_product p on od.id_product = p.id_product
-join eco_category ca on p.id_category = ca.id_category
-group by ca.category_name
+    eco_category.category_name,
+    sum(eco_order_detail.quantity * eco_order_detail.historical_price) as total_sales
+from eco_order_detail
+join eco_product
+    on eco_order_detail.id_product = eco_product.id_product
+join eco_category
+    on eco_product.id_category = eco_category.id_category
+group by eco_category.category_name
 order by total_sales desc;
 
--- Query 4: Products with the lowest inventory
+-- query 4: products with the lowest inventory
 select
     product_name,
     stock
@@ -33,19 +37,20 @@ from eco_product
 order by stock asc
 limit 5;
 
--- Query 5: Customers with the highest number of orders
+-- query 5: customers with the highest number of orders
 select
-    cu.customer_name,
-    count(o.id_order) as total_orders
-from eco_customer cu
-join eco_order o on cu.id_customer = o.id_customer
-group by cu.customer_name
+    eco_customer.customer_name,
+    count(eco_order.id_order) as total_orders
+from eco_customer
+join eco_order
+    on eco_customer.id_customer = eco_order.id_customer
+group by eco_customer.customer_name
 order by total_orders desc;
 
--- Query 6: Inventory value by distribution center
+-- query 6: inventory value by distribution center
 select
-    dc.distribution_center_name,
-    sum(p.stock * p.unit_price) as inventory_value
-from eco_distribution_center dc
-cross join eco_product p
-group by dc.distribution_center_name;
+    eco_distribution_center.distribution_center_name,
+    sum(eco_product.stock * eco_product.unit_price) as inventory_value
+from eco_distribution_center
+cross join eco_product
+group by eco_distribution_center.distribution_center_name;
